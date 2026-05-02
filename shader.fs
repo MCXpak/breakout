eb43c514407c51e3df430289b420472d20df6ade
MCXpak <mouradlasga@gmail.com>
2026-05-02 17:23:28 +0100

    working directional and point lights

--- shader.fs
+++ shader.fs
@@ -1,26 +1,111 @@
 #version 330 core
 out vec4 FragColor;
+
+struct DirLight {
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+struct PointLight {
+    vec3 position;
+    
+    float constant;
+    float linear;
+    float quadratic;
+	
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
   
-in vec3 ourColor;
+in vec3 FragPos;
 in vec2 TexCoord;
-in vec3 pos;
+in vec3 Normal;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 uniform float aMix;
+uniform DirLight dirLight;
+uniform PointLight pointLight;
+uniform PointLight pointLights[2];
+uniform vec3 viewPos;
+uniform vec3 objectColor;
 
 const vec4 innerColor = vec4(0.5, 0.0, 0.0, 1.0);
 const vec2 center = vec2(0.5, 0.5);
 const float cutoffHalfSize = 0.4;
 
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+
 void main()
 {
+    // properties
+    vec3 norm = normalize(Normal);
+    vec3 viewDir = normalize(viewPos - FragPos);
+
+    // Calc color
     //Chebyshev distance calculation
     vec2 offset = abs(TexCoord - center);
     float maxDist = max(offset.x, offset.y);
     float factor = step(cutoffHalfSize, maxDist);
+    vec4 color = mix(vec4(vec3(1.0, 0.0, 0.0), 1.0), vec4(0.0), factor);
+
+    // directional lighting
+    vec3 result = vec3(0.0);
+    result = CalcDirLight(dirLight, norm, viewDir);
+
+    // point light
+    for (int i = 0; i < 2; i++)
+        result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);
+    //result += objectColor;
 
     //float factor = clamp(distance(TexCoord.xy, vec2(0.5,0.5)) / 0.7071, 0.0, 1.0);
     //FragColor = mix(texture(texture1, vec2(TexCoord.x, TexCoord.y) ), texture(texture2, vec2(TexCoord.x, TexCoord.y)), aMix);
-    FragColor = mix(vec4(ourColor, 1.0), vec4(0.0), factor);
+    
+    FragColor = vec4(result, 1.0);
+    //FragColor = vec4(0.4,0.2,0.2, 1.0);
+}
+
+// calculates the color when using a directional light.
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
+{
+    vec3 lightDir = normalize(-light.direction);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 0.5);
+    // combine results
+    vec3 ambient = light.ambient * objectColor;
+    vec3 diffuse = light.diffuse * diff * objectColor;
+    vec3 specular = light.specular * spec * objectColor;
+    return (ambient + diffuse + specular);
+}
+
+// calculates the color when using a point light.
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+    vec3 lightDir = normalize(light.position - fragPos);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = max(dot(viewDir, reflectDir), 0.0);
+    // attenuation
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+    // combine results
+    vec3 ambient = light.ambient * objectColor;
+    vec3 diffuse = light.diffuse * diff * objectColor;
+    vec3 specular = light.specular * spec * objectColor;
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+    return (ambient + diffuse + specular);
+    //return ambient;
+
 }
